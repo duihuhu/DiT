790af6d1b7508e0bada59917e97ae03ddec92eaf
duihuhu <duihuhu@163.com>
2024-08-12 16:46:58 +0800

    debug add debug info

--- diffusion/gaussian_diffusion.py
+++ diffusion/gaussian_diffusion.py
@@ -503,6 +503,7 @@ class GaussianDiffusion:
             indices = tqdm(indices)
         for i in indices:
             t = th.tensor([i] * shape[0], device=device)
+            print(i)
             with th.no_grad():
                 out = self.p_sample(
                     i,
