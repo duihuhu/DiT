d782c7c1f39de0e9e239e2c1f9a94ddb24e881ed
duihuhu <duihuhu@163.com>
2024-08-13 15:42:50 +0800

    debug add debug info

--- diffusion/gaussian_diffusion.py
+++ diffusion/gaussian_diffusion.py
@@ -503,21 +503,20 @@ class GaussianDiffusion:
 
             indices = tqdm(indices)
         for i in indices:
-            if i > 2:
-                # t = th.tensor([i] * shape[0], device=device)
-                # with th.no_grad():
-                #     out = self.p_sample(
-                #         model,
-                #         img,
-                #         t,
-                #         clip_denoised=clip_denoised,
-                #         denoised_fn=denoised_fn,
-                #         cond_fn=cond_fn,
-                #         model_kwargs=model_kwargs6,
-                #     )
-                #     yield out
-                #     img = out["sample"]
-                continue
+            if i > 50:
+                t = th.tensor([i] * shape[0], device=device)
+                with th.no_grad():
+                    out = self.p_sample(
+                        model,
+                        img,
+                        t,
+                        clip_denoised=clip_denoised,
+                        denoised_fn=denoised_fn,
+                        cond_fn=cond_fn,
+                        model_kwargs=model_kwargs6,
+                    )
+                    yield out
+                    img = out["sample"]
             else:
                 t = th.tensor([i] * shape[0], device=device)
                 with th.no_grad():
